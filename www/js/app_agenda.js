b2e6ad488be790dcbed364dd892264069bc9be68
badragon <alberto.mazza@webdevhell.com>
2014-01-05 18:55:36 +0100

    update

---
+++ www/js/app_agenda.js
@@ -0,0 +1,15 @@
+var agenda = {
+    // Application Constructor
+    initialize: function() {
+        console.log('Agenda initialized.');
+    	navigator.notification.alert("Utente: " + app.userid, function() {});
+    	
+    	$("#content_agenda").html("Loading...");
+    	agenda.getAgenda();
+    },
+    
+    getAgenda: function() {
+    	$("#content_agenda").html("Loaded");
+    }
+    
+};
